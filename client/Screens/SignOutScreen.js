83422c61b33f9e77f5b8839a0c6a366bda8fc09d
Sherman <shc410@gmail.com>
2016-10-25 19:41:02 -0700

    make some progress on navbar issue, will submit pull request for now so others can try a solution

--- client/Screens/SignOutScreen.js
+++ client/Screens/SignOutScreen.js
@@ -8,12 +8,22 @@ export default class SignOutScreen extends Component {
     super();
   }
 
+  // verify if user really wants to log out
   signOutPressHandler() {
     console.log('signout wired');
     // not redirecting correctly, it just replaces the screen with the newly routed screen, doesnt actually redirect
-    // this.props.navigator.replace(Router.getRoute('Login'));
+    // this.props.navigator.push(Router.getRoute('Login'));
+    
+    // var routeOfLogin = {
+    //   title: 'Login',
+    //   component: Router.getRoute('Login'),
+    //   navigationBarHidden: true
+    // };
+    // this.props.navigator.push(Router.goToView(routeOfLogin));
+
   }
 
+  // returns users to homepage if logout icon was accidentally clicked, they can also use the navbar this is just a convenience
   goBackPressHandler() {
     console.log('goback wired');
   }
