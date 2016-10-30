0730d0297f08cb83e614cc47ed96b3ddb40c4583
Sherman <shc410@gmail.com>
2016-10-30 10:48:03 -0700

    working out how to use enzyme

--- spec/loginScreenSpec.js
+++ spec/loginScreenSpec.js
@@ -1,13 +1,13 @@
-import React, { Component, PropTypes } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { shallow } from 'enzyme';
-import loginScreen from './Screens/LoginScreen.js';
-import { expect } from 'chai';
+// import React, { Component, PropTypes } from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import { shallow } from 'enzyme';
+// import loginScreen from './Screens/LoginScreen.js';
+// import { expect } from 'chai';
 
-describe('Login Screen', () => {
-  it('should render stuff', () => {
-    const wrapper = shallow(<LoginScreen />);
-    expect(wrapper.length).to.equal(1);
-    // expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
-  });
-});
+// describe('Login Screen', () => {
+//   it('should render stuff', () => {
+//     const wrapper = shallow(<LoginScreen />);
+//     expect(wrapper.length).to.equal(1);
+//     // expect(wrapper.contains(<Text>I wonder if there will be any problems...</Text>)).to.equal(true);
+//   });
+// });
